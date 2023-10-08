c8d84164218ccf4cbd3a490b300834e41606a6dd
Ception <ception@live.ca>
2023-10-08 06:24:14 -0400

    fixed header, finally done

--- src/app/(components)/Header.js
+++ src/app/(components)/Header.js
@@ -1,62 +1,43 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { animateScroll } from "react-scroll";
 
 export default function Header() {
-  const router = useRouter();
-
   function navigateToHome() {
-    // router.push("/");
-    animateScroll.scrollToTop();
+    animateScroll.scrollToTop({
+      duration: 100,
+      smooth: "easeInOutQuint",
+    });
   }
 
   return (
-    <header className="fixed top-0 left-0 w-full p-4 bg-red-700 text-slate-200 drop-shadow-xl z-50">
-      <nav className="container mx-auto flex items-center justify-between flex-wrap p-4">
-        <div className="flex items-center flex-no-shrink text-slate-200 mr-6 cursor-pointer transform hover:scale-105 transition-transform duration-500">
+    <header className="fixed top-0 left-0 w-full bg-red-700 text-slate-200 z-50 transition-all duration-300 ease-in-out shadow-lg">
+      <nav className="container mx-auto flex items-center justify-between p-4">
+        <div className="flex items-center flex-no-shrink text-slate-200 cursor-pointer transform hover:scale-105 transition-transform duration-500">
           <span
-            className="text-3xl font-bold tracking-wider italic"
+            className="text-4xl font-extrabold tracking-tight italic"
             onClick={navigateToHome}
           >
             SQC
           </span>
         </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-lg lg:flex-grow flex justify-end space-x-4 items-center">
-            <Link
-              href="#about-us"
-              className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-yellow-200 transition-colors duration-300 hover:shadow-md p-2 rounded"
-            >
-              About Us
-            </Link>
-            <Link
-              href="#services"
-              className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-yellow-200 transition-colors duration-300 hover:shadow-md p-2 rounded"
-            >
-              Services
-            </Link>
-            <Link
-              href="#gallery"
-              className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-yellow-200 transition-colors duration-300 hover:shadow-md p-2 rounded"
-            >
-              Gallery
-            </Link>
-            <Link
-              href="#our-process"
-              className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-yellow-200 transition-colors duration-300 hover:shadow-md p-2 rounded"
-            >
-              Our Process
-            </Link>
-            <Link
-              className="inline-block px-8 py-4 text-slate-200 hover:text-black tracking-tighter hover:bg-green-400 border-2 border-white focus:border-green-400 focus:border-opacity-40 hover:border-green-400 focus:ring-4 focus:ring-green-400 focus:ring-opacity-40 rounded-full transition duration-300"
-              href="#contact-us"
-            >
-              Get in Touch
-            </Link>
-          </div>
+        <div className="hidden lg:flex flex-grow items-center justify-center space-x-4">
+          {["#about-us", "#services", "#gallery", "#our-process"].map(
+            (href) => (
+              <Link key={href} href={href} passHref>
+                <span className="cursor-pointer text-slate-200 hover:bg-white hover:text-red-700 transition-colors duration-300 p-2 rounded-lg text-lg">
+                  {href.slice(1).replace("-", " ")}
+                </span>
+              </Link>
+            )
+          )}
         </div>
+        <Link href="#contact-us" passHref>
+          <span className="cursor-pointer inline-block px-8 py-2 text-slate-200 text-lg font-medium bg-green-500 border border-transparent hover:bg-slate-700 focus:border-green-500 focus:border-opacity-40 focus:ring-4 focus:ring-green-500 focus:ring-opacity-40 rounded-lg transition duration-300">
+            Get in Touch
+          </span>
+        </Link>
       </nav>
     </header>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,15 @@
-import '@/styles/global.css'
+import "@/styles/global.css";
 
 export const metadata = {
-  title: 'Searle Construction',
-  description: 'Searle Construction is a family owned and operated business...',
-}
- 
+  title: "Searle Quality Contracting",
+  description:
+    "SQC has proudly been serving Halton region and surrounding areas for over 5 yrs, providing quality craftsmanship and a high attention to detail. From inital quote , to planning , and then execution of the project; SQC ensures transparent communication and professionalism . Thus making your renovation process as easy as possible for you.",
+};
+
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-        <body>{children}</body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
