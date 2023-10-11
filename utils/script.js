6f8dbf848b2d0845e70d167b88dbfff5db30b83f
Daniel_Medina <Medinald@sheridancollege.ca>
2023-10-11 13:58:50 -0400

    added email logic | mailgunjs

--- utils/script.js
+++ utils/script.js
@@ -1,4 +1,21 @@
-export function sendContactForm(name, email, date, message) {
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({username: 'api', key: '48d68a0a532685bcdb68a083db6f4a73-262b213e-661603a9'});
+
+
+export function sendContactForm(email, name, date, message) {
     console.log('submitting contact form');
     console.log(name, email, date, message);
+    console.log(formData);
+    mg.messages.create('sandboxec35ff63f0604609bc91f0944acb66d1.mailgun.org', {
+        from: `${email} <mailgun@sandbox-123.mailgun.org>`,
+        to: ["medina.khe@gmail.com"],
+        subject: `New Booking Request | ${name}`,
+        text: `${message} | ${date}`,
+        html: `<h1>Testing some Mailgun awesomeness!</h1><br /><p> ${message} | ${date} </p>`
+    })
+    .then(msg => console.log(msg)) // logs response data
+    .catch(err => console.log(err)); // logs any error
 }
+
